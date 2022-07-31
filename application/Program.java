61ec6a19cb8d769b954a1900fdcba13d7fa8ae65
engenheiro.muniz <engenheiro.muniz@gmail.com>
2022-07-31 20:13:21 -0300

    Creating SellerDao and DaoFactory

--- application/Program.java
+++ application/Program.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.swing.JOptionPane;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,6 +17,7 @@ public class Program {
 		Department obj    = new Department(1, "Books");
 		Seller     seller = new Seller(35, "Ana Maria Braga", "ana.maria@globo.com", new Date(), 2000.0, obj); 
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		JOptionPane.showMessageDialog(null, "Department: "+obj+"\nSeller: "+seller, "Print obj element",
 				JOptionPane.PLAIN_MESSAGE );
